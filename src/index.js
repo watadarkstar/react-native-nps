820fd2520af17143255512a6dcf4e9aa03643a70
Adrian Carolli <adrian.carolli1@gmail.com>
2019-01-09 19:22:29 -0500

    Link rn-svg and style the popup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Planet } from 'react-native-kawaii';
 
 export default class NPS extends Component {
   renderPopup = () => (
     <View style={styles.popup}>
-      <Text>test</Text>
+      <Text style={styles.title}>How likely are you to recommend our app?</Text>
+      <View style={styles.icons}>
+        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="sad" color="#FF6347" style={styles.icon} /></TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="sad" color="#FF6347" style={styles.icon} /></TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="sad" color="#FF6347" style={styles.icon} /></TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="sad" color="#FF6347" style={styles.icon} /></TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="sad" color="#FF6347" style={styles.icon} /></TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="sad" color="#FF6347" style={styles.icon} /></TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="happy" color="#FCCB7E" style={styles.icon} /></TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="happy" color="#FCCB7E" style={styles.icon} /></TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="blissful" color="#9ACD32" style={styles.icon} /></TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="blissful" color="#9ACD32" style={styles.icon} /></TouchableOpacity>
+      </View>
+      <View style={styles.iconsFooter}>
+        <Text style={styles.iconsFooterText}>Not Likely</Text>
+        <Text style={styles.iconsFooterText}>Very Likely</Text>
+      </View>
+      <Text style={styles.subtitle}>What could we do to improve?</Text>
+      <TextInput
+        style={styles.input}
+        multiline={true}
+        numberOfLines={4}
+      />
     </View>
   );
 
@@ -35,10 +58,50 @@ const styles = StyleSheet.create({
   },
   popup: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
     position: "absolute",
     backgroundColor: "white",
     width: 350,
-    padding: 20,
+    paddingTop: 40,
+    paddingBottom: 25,
+    paddingLeft: 25,
+    paddingRight: 25,
     paddingBottom: 25
   },
+  icons: {
+    flexDirection: "row",
+  },
+  iconsFooter: {
+    flex: 1,
+    width: 350,
+    paddingLeft: 17,
+    paddingRight: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  iconsFooterText: {
+    textAlign: "center",
+  },
+  icon: {},
+  title: {
+    paddingBottom: 20
+  },
+  subtitle: {
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  iconContainer: {
+    paddingLeft: 3
+  },
+  input: {
+    flex: 1,
+    height: 90,
+    width: 300,
+    marginBottom: 20,
+    borderColor: '#ddd',
+    borderWidth: 1
+  }
 });

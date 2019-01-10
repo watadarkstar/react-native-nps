56456e48c09d0a41cbeca8d18cce44e472f4d803
Adrian Carolli <adrian.carolli1@gmail.com>
2019-01-09 19:47:37 -0500

    improve styles and format

--- src/index.js
+++ src/index.js
@@ -7,16 +7,36 @@ export default class NPS extends Component {
     <View style={styles.popup}>
       <Text style={styles.title}>How likely are you to recommend our app?</Text>
       <View style={styles.icons}>
-        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="sad" color="#FF6347" style={styles.icon} /></TouchableOpacity>
-        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="sad" color="#FF6347" style={styles.icon} /></TouchableOpacity>
-        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="sad" color="#FF6347" style={styles.icon} /></TouchableOpacity>
-        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="sad" color="#FF6347" style={styles.icon} /></TouchableOpacity>
-        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="sad" color="#FF6347" style={styles.icon} /></TouchableOpacity>
-        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="sad" color="#FF6347" style={styles.icon} /></TouchableOpacity>
-        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="happy" color="#FCCB7E" style={styles.icon} /></TouchableOpacity>
-        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="happy" color="#FCCB7E" style={styles.icon} /></TouchableOpacity>
-        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="blissful" color="#9ACD32" style={styles.icon} /></TouchableOpacity>
-        <TouchableOpacity style={styles.iconContainer}><Planet size={30} mood="blissful" color="#9ACD32" style={styles.icon} /></TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}>
+          <Planet size={28} mood="sad" color="#FF6347" style={styles.icon} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}>
+          <Planet size={28} mood="sad" color="#FF6347" style={styles.icon} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}>
+          <Planet size={28} mood="sad" color="#FF6347" style={styles.icon} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}>
+          <Planet size={28} mood="sad" color="#FF6347" style={styles.icon} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}>
+          <Planet size={28} mood="sad" color="#FF6347" style={styles.icon} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}>
+          <Planet size={28} mood="sad" color="#FF6347" style={styles.icon} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}>
+          <Planet size={28} mood="happy" color="#FCCB7E" style={styles.icon} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}>
+          <Planet size={28} mood="happy" color="#FCCB7E" style={styles.icon} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}>
+          <Planet size={28} mood="blissful" color="#9ACD32" style={styles.icon} />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconContainer}>
+          <Planet size={28} mood="blissful" color="#9ACD32" style={styles.icon} />
+        </TouchableOpacity>
       </View>
       <View style={styles.iconsFooter}>
         <Text style={styles.iconsFooterText}>Not Likely</Text>
@@ -27,7 +47,11 @@ export default class NPS extends Component {
         style={styles.input}
         multiline={true}
         numberOfLines={4}
+        placeholder={"I choose X because..."}
       />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
     </View>
   );
 
@@ -75,33 +99,52 @@ const styles = StyleSheet.create({
   iconsFooter: {
     flex: 1,
     width: 350,
-    paddingLeft: 17,
-    paddingRight: 10,
+    paddingLeft: 25,
+    paddingRight: 18,
     paddingTop: 10,
     paddingBottom: 10,
     flexDirection: "row",
     justifyContent: "space-between",
   },
   iconsFooterText: {
+    fontSize: 13,
     textAlign: "center",
   },
   icon: {},
   title: {
-    paddingBottom: 20
+    fontSize: 15,
+    paddingBottom: 12
   },
   subtitle: {
+    fontSize: 15,
     paddingTop: 10,
-    paddingBottom: 10
+    paddingBottom: 12
   },
   iconContainer: {
     paddingLeft: 3
   },
   input: {
     flex: 1,
+    paddingTop: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
     height: 90,
     width: 300,
     marginBottom: 20,
     borderColor: '#ddd',
     borderWidth: 1
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+    fontWeight: "bold"
+  },
+  button: {
+    flex: 1,
+    backgroundColor: "#1da1f2",
+    width: 300,
+    padding: 15,
+    marginBottom: 20
   }
 });
